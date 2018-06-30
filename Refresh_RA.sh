5988547f99b00504f36707308c2819df911e551c
EthanR12 <flubixcube@hotmail.com>
2018-06-29 18:47:39 -0700

    bug fix

--- Refresh_RA.sh
+++ Refresh_RA.sh
@@ -9,7 +9,7 @@ for i in $(ls /$short); do
 		location=`echo $(dirname $match) | tail -c4`
 		final=`find /$short/$i/$location -maxdepth 0 -type d`
 		if [ ! -f /tmp/"$i"ra_cache ]; then cat /$short/$i/*/*/*.desktop | awk '/^Exec=/{print}' | grep "CLV" \
-		| sed -n -e 's/^.*\(CLV-.-.\{5\}\/\)/\1/p' | awk '{print $1}' > /tmp/"$i"ra_cache; fi
+		| sed -n -e 's/^.*\(CLV-.-.\{5\}\/\)/\1/p' | awk '{print $1}' | sort -u > /tmp/"$i"ra_cache; fi
 		mv $final/CLV-S-00000 /$short/CLV-S-00000
 		find $final/* -maxdepth 0 -type d 2>/dev/null | while read rmloop; do
 			if [[ "$rmloop" == $final/CLV-L-* ]] || [[ "$rmloop" == $final/CLV-S-* ]]; then :; else rm -rf $rmloop; fi
