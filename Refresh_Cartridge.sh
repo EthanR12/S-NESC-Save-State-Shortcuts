2bfcc83228cd734b9d6967c1ddbc9e7e219dacc7
EthanR12 <flubixcube@hotmail.com>
2018-07-04 10:13:02 -0700

    restructure and prepare for RA

--- Refresh_Cartridge.sh
+++ Refresh_Cartridge.sh
@@ -5,7 +5,10 @@
 
 if [ -d /media/hakchi ]; then media=media; else media=var/lib; fi
 if [ -d /media/hakchi ]; then savesloc=media/hakchi/saves; else savesloc=var/lib/clover/profiles/0; fi
+find /$savesloc -maxdepth 2 \( -name "*.sram*" -o -name "*.png" -o -name "suspend*" -type d \) 2>/dev/null > /tmp/saves.txt
 for i in $(ls /$media/hakchi/games); do
+	if echo $i | grep -q "^s"; then cat /tmp/saves.txt | grep "cartridge" | grep -o "CLV-.-.\{5\}" | sort -u > \
+	/tmp/folder.txt; else cat /tmp/saves.txt | grep "save.sram" | grep -o "CLV-.-.\{5\}" | sort -u > /tmp/folder.txt; fi
 	grep -wril "name=sram saves" /$media/hakchi/games/$i/*/CLV-S-*/*.desktop 2>/dev/null | while read match; do
 		location=`echo $(dirname $match) | tail -c4`
 		final=`find /$media/hakchi/games/$i/$location -maxdepth 0 -type d`
@@ -14,10 +17,9 @@ for i in $(ls /$media/hakchi/games); do
 			if [[ "$rmloop" == $final/CLV-L-* ]] || [[ "$rmloop" == $final/CLV-S-* ]]; then :; else rm -rf $rmloop; fi
 		done
 		mv /$media/hakchi/games/CLV-S-00000 $final/CLV-S-00000
-		find /$savesloc -maxdepth 2 -name "*.sram" 2>/dev/null | while read file; do
-			found="$(basename $(dirname $file))"
-			find /$media/hakchi/games/$i/*/$found -maxdepth 0 -type d 2>/dev/null | while read copy; do
-				if [ -d $final/$found ]; then :; else cp -r $copy $final; fi
+		cat /tmp/folder.txt | while read j; do
+			find /$media/hakchi/games/$i/*/$j -maxdepth 0 -type d 2>/dev/null | while read copy; do
+				if grep -ql " (#)(" $copy/$j.desktop; then :; else if [ -d $final/$j ]; then :; else cp -rv $copy $final; fi; fi
 			done
 		done
 	done
