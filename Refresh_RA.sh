7eee1279be056d060fd571f2e523182646b75f2b
EthanR12 <flubixcube@hotmail.com>
2018-07-13 15:17:52 -0700

    Reformat for better readability and dualboot support

--- Refresh_RA.sh
+++ Refresh_RA.sh
@@ -5,27 +5,47 @@
 
 short=media/hakchi/games
 for i in $(ls /$short); do
-	grep -wril "name=ra saves" /$short/$i/*/CLV-S-*/*.desktop | while read match; do
-		location=`echo $(dirname $match) | tail -c4`
-		final=`find /$short/$i/$location -maxdepth 0 -type d`
-		if [ ! -f /tmp/"$i"ra_cache ]; then cat /$short/$i/*/*/*.desktop | awk '/^Exec=/{print}' | grep "CLV" \
-		| sed -n -e 's/^.*\(CLV-.-.\{5\}\/\)/\1/p' | awk '{print $1}' | sort -u > /tmp/"$i"ra_cache; fi
-		mv $final/CLV-S-00000 /$short/CLV-S-00000
-		find $final/* -maxdepth 0 -type d 2>/dev/null | while read rmloop; do
-			if [[ "$rmloop" == $final/CLV-L-* ]] || [[ "$rmloop" == $final/CLV-S-* ]]; then :; else rm -rf $rmloop; fi
-		done
-		mv /$short/CLV-S-00000 $final/CLV-S-00000
-		for j in $(ls /media/data/ra_savestates); do
-			number=`echo $j | sed -n -e 's/^.*state//p'`
-			game="${j%.*}"
-			folder=`grep "$game" /tmp/"$i"ra_cache | sed 's/^\(.\{11\}\).*/\1/'`
-			if [ ! -d $final/$folder ]; then
-				find /$short/$i/*/$folder -maxdepth 0 -type d 2>/dev/null | while read copy; do
-					cp -r $copy $final
-				done
-			fi
-			if [ -f $final/$folder/*.desktop ]; then cat $final/$folder/$folder.desktop | \
-			sed "/^Name=/ s/$/ ("$number")/" > tmpfile.txt && mv tmpfile.txt $final/$folder/$folder.desktop; fi
-		done
-	done
+
+if echo $i | grep -q "^s"; then :; else if [ ! -f /tmp/"$i"_racache.txt ]; then \
+cat /$short/$i/*/*/*.desktop | awk '/^Exec=/{print}' | sed -n 's/^.*\(CLV-.-.\{5\}\/\)/\1/p' \
+| awk '{print $1}' | sort -u > /tmp/"$i"_racache.txt; fi; fi
+
+find /media/data/ra_savestates -name "*state*" | sed -n 's/^.*_savestates\///p' > /tmp/rasave.txt
+
+if echo $i | grep -q "^s"; then cat /tmp/rasave.txt | grep "CLV" \
+| sed -n "s/.state/ /p" | sort -k1,1 -k2n > /tmp/"$i"_saves.txt; else cat /tmp/rasave.txt \
+| grep -v "CLV" | sed -n "s/.state/ /p" | sort -k1,1 -k2n > /tmp/"$i"_saves.txt; fi
+
+grep -wril "name=ra saves" /$short/$i/*/CLV-S-*/*.desktop | while read match; do
+
+location=`echo $(dirname $match) | tail -c4`
+final=`find /$short/$i/$location -maxdepth 0 -type d`
+
+mv $final/CLV-S-00000 /$short/CLV-S-00000
+
+find $final/* -maxdepth 0 -type d 2>/dev/null | while read rmloop; do
+	if [[ "$rmloop" == $final/CLV-L-* ]] || [[ "$rmloop" == $final/CLV-S-* ]]; then :; else rm -rf $rmloop; fi
+done
+
+mv /$short/CLV-S-00000 $final/CLV-S-00000
+
+cat /tmp/"$i"_saves.txt | while read j; do
+
+number=`echo $j | sed -n 's/^.* //p'`
+game="${j% *}"
+
+if echo $i | grep -q "^s"; then folder=`grep "$game" /tmp/"$i"_saves.txt | grep -o "CLV-.-.\{5\}" \
+| sort -u`; else folder=`grep "/$game[\.$]" /tmp/"$i"_racache.txt | grep -o "CLV-.-.\{5\}"`; fi
+
+if echo $folder | grep -q " "; then folder=`echo $folder | grep -o "CLV-U-.\{5\}"`; fi
+
+find /$short/$i/*/$folder -maxdepth 0 -type d 2>/dev/null | while read copy; do
+	if [ -d $final/$folder ]; then :; else cp -r $copy $final; fi
+done
+
+cat $final/$folder/$folder.desktop 2>/dev/null | sed "/^Name=/ s/$/ ("$number")/" \
+> tmp.txt && mv tmp.txt $final/$folder/$folder.desktop
+
+done
+done
 done
